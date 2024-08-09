1af0bf4a923b4cd5aeca52c76a875b948826853c
vandyand <vandyand@gmail.com>
2024-08-09 13:21:08 -0400

    added delete_csvs.py for csv cleanup

--- src/main.sh
+++ src/main.sh
@@ -12,3 +12,4 @@ python src/auction_data/get_auction_data.py
 python src/items_data/get_auction_items_csvs.py
 python src/items_data/items_data_ingest.py
 python src/items_data/main.py
+python src/items_data/csvs/delete_csvs.py
